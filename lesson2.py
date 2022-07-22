b0839c34559a75c7e4f2fa05dbbc00508e430b93
erikaatamova <105799084+erikaatamova@users.noreply.github.com>
2022-07-22 10:23:29 -0500

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+my_string="Test Automation"
+print(my_string[0:5])
+
+word_test="test"
+print(word_test)
+
+my_string="Test Automation"
+second_word="Automation"
+print(second_word)
+
+
+my_list=[10,50,20,25,30]
+sliced_list=[50,20]
+print(sliced_list)
